b753924af2712981a868050a087b2b33ef39bbb8
isabellerc <116124929+isabellerc@users.noreply.github.com>
2023-05-04 19:36:38 -0300

    Update Program.cs

--- Missão 2/Program.cs
+++ Missão 2/Program.cs
@@ -10,14 +10,22 @@ for (int i = 0; i < listaNomes.Length; i++)
     listaNomes[i] = Console.ReadLine();
 }
 
+
+Random rnd = new Random();
+
+List<int> indicesAleatorios = Enumerable.Range(0, listaNomes.Length).OrderBy(x => rnd.Next()).ToList();
+
 Console.WriteLine("Resultado do amigo secreto:");
-for (int i = 0; i < 1; i++)
+
+for (int i = 0; i < listaNomes.Length; i++)
 {
-    Console.WriteLine(listaNomes[0] + " -> " + listaNomes[5]);
-    Console.WriteLine(listaNomes[1] + " -> " + listaNomes[4]);
-    Console.WriteLine(listaNomes[2] + " -> " + listaNomes[7]);
-    Console.WriteLine(listaNomes[3] + " -> " + listaNomes[6]);
+    int amigoSecretoIndex = (i + 1) % listaNomes.Length;
+    if (amigoSecretoIndex == i) amigoSecretoIndex = (i + 2) % listaNomes.Length;
+
+  
+    Console.WriteLine(listaNomes[i] + " -> " + listaNomes[indicesAleatorios[amigoSecretoIndex]]);
 }
+
 
 
 
